53188b00940766723d9e6d77b89f195afbb7113c
GaoxingAbdullah <abduldoobia@yahoo.com>
2023-02-01 15:52:48 +0800

    Changes

--- cnp/admin.py
+++ cnp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'location']
+    list_display = ['poster', 'title', 'description', 'eventDate', 'eventTime', 'location', 'date_created']
+    list_filter = ['eventDate', 'eventTime', 'location']
+    
+admin.site.register(Event, EventAdmin)
--- cnp/models.py
+++ cnp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+    poster = models.ImageField(max_length=200, null=True)
+    eventDate = models.DateField(null=True)
+    eventTime = models.TimeField(null=True)
+    location = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
